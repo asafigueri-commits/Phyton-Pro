814b5e8738932aff364052ad97957383c97b15c1
asafigueri-commits <asafigueri@gmail.com>
2026-08-20 18:18:41 -0600

    Implement password generator in Phyton.py
    Added password generation functionality using random characters.

--- Phyton.py
+++ Phyton.py
@@ -15,3 +15,19 @@ for i in range(5):
         print(word, "significa:",meme_dict[word])
     else:
         print(word, "no esta en el diccionario... ")
+
+
+
+
+
+import random
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+longitud = int(input("Ingrese la longitud de la contraseña.          "))
+
+contrasena_generada =  ""
+
+for i in range(longitud):
+    contrasena_generada += random.choice(caracteres)
+    print("")
+
+print("Tu contrasena generada es:", contrasena_generada)
